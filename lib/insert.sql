8601f6d93ae202db8b0f23e8c6e7e49b0577e961
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-06-25 13:14:02 -0500

    commit tree d289bc9b0edfacf572adc73478b074744f09b71d

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 15, 'F', 'Black', 'Nice', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 15, 'F', 'Black', 'Nice', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 15, 'F', 'Black', 'Nice', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 15, 'F', 'Black', 'Nice', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 15, 'F', 'Black', 'Nice', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 15, 'F', 'Black', 'Nice', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 15, 'F', 'Black', 'Nice', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 15, 'F', 'Black', 'Nice', true);
